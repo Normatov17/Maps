786ef8ae163bf488e2ea6494cbe40f73b35b5269
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-06-22 02:26:49 +0500

    popup

--- js/index.js
+++ js/index.js
@@ -27,10 +27,40 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
-      const { longited } = position.coords;
-      console.log(
-        `https://www.google.com/maps/@54.8699365,37.2740523,11.97z?entry=ttu ${latitude}`
-      );
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map("map").setView(coords, 12);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // L.marker(coords);
+      // addTo(map);
+      // bindPopup("A pretty CSS3 popup.<br>Easily customizable");
+      // openPopup();
+
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnclick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
 
     function () {
